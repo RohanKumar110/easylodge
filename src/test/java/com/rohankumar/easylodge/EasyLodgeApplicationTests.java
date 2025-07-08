ebf7a2f07d4ded7805702bf6eba1ce2d49dd4bb3
rohankumar110 <rohanktalreja110@gmail.com>
2025-07-07 23:49:35 -0400

    init project

---
+++ src/test/java/com/rohankumar/easylodge/EasyLodgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohankumar.easylodge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLodgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
