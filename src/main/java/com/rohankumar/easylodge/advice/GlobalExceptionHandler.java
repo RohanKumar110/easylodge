5c4874ff4209596b5418147b620a3d03bcdfeccd
rohankumar110 <rohanktalreja110@gmail.com>
2025-07-24 01:06:21 -0400

    handle other exceptions in exception handler

--- src/main/java/com/rohankumar/easylodge/advice/GlobalExceptionHandler.java
+++ src/main/java/com/rohankumar/easylodge/advice/GlobalExceptionHandler.java
@@ -3,20 +3,22 @@ package com.rohankumar.easylodge.advice;
 import com.rohankumar.easylodge.dtos.wrapper.ErrorResponse;
 import com.rohankumar.easylodge.exceptions.BadRequestException;
 import com.rohankumar.easylodge.exceptions.ResourceNotFoundException;
+import com.rohankumar.easylodge.exceptions.TokenExpiredException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+import org.springframework.security.core.AuthenticationException;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
@@ -42,15 +44,6 @@ public class GlobalExceptionHandler {
                 ErrorResponse.error(BAD_REQUEST.value(), "Validation Error", errors));
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
-
-        log.warn("Resource Not Found Error: {}", ex.getMessage());
-
-        return ResponseEntity.status(NOT_FOUND).body(
-                ErrorResponse.error(NOT_FOUND.value(), ex.getMessage()));
-    }
-
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
 
@@ -67,6 +60,82 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(BAD_REQUEST).body(
                 ErrorResponse.error(BAD_REQUEST.value(), "Invalid email or password"));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException ex) {
+
+        log.warn("Missing Servlet Request Parameter Error: "+ex.getMessage());
+
+        String message = "Required Parameter '" + ex.getParameterName() + "' is Missing";
+        return ResponseEntity.status(BAD_REQUEST).body(
+                ErrorResponse.error(BAD_REQUEST.value(), message));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException ex) {
+
+        log.warn("Http Request Method Not Supported Error: "+ex.getMessage());
+
+        String message = "Request method '" + ex.getMethod() + "' is not supported for this Endpoint";
+        return ResponseEntity.status(BAD_REQUEST).body(
+                ErrorResponse.error(BAD_REQUEST.value(), message));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException ex) {
+
+        log.warn("No Resource Found Error: {}", ex.getMessage());
+
+        return ResponseEntity.status(NOT_FOUND).body(
+                ErrorResponse.error(NOT_FOUND.value(),"The Requested Resource was not Found"));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+
+        log.warn("Resource Not Found Error: {}", ex.getMessage());
+
+        return ResponseEntity.status(NOT_FOUND).body(
+                ErrorResponse.error(NOT_FOUND.value(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<ErrorResponse> handleTokenExpiredException(TokenExpiredException ex) {
+
+        log.warn("Token Expired Error: {}", ex.getMessage());
+
+        return ResponseEntity.status(UNAUTHORIZED).body(
+                ErrorResponse.error(UNAUTHORIZED.value(), "Session Expired"));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ErrorResponse> handleJwtException(JwtException ex) {
+
+        log.warn("Jwt Error: {}", ex.getMessage());
+
+        return ResponseEntity.status(UNAUTHORIZED).body(
+                ErrorResponse.error(UNAUTHORIZED.value(), "Not Authorized"));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(
+            AuthenticationException ex, HttpServletRequest req) {
+
+        log.warn("Authentication Error: {}", ex.getMessage());
+
+        return ResponseEntity.status(UNAUTHORIZED).body(
+                ErrorResponse.error(UNAUTHORIZED.value(), "Authentication Required"));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+
+        log.info("Access Denied Error: "+ex.getMessage());
+        return ResponseEntity.status(FORBIDDEN).body(
+                ErrorResponse.error(FORBIDDEN.value(), "Access Denied"));
     }
 
     @ExceptionHandler(Exception.class)
